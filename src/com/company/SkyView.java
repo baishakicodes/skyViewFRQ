c7989738606d775837c508e7c76d78a3bbbe38c2
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2019-01-15 19:10:15 -0500

    need to figure out how to print the 2d arrays and why there is an error with the constructor.

--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -2,20 +2,43 @@ package com.company;
 
 public class SkyView {
     private double[][] view;
+
     public SkyView(int numRows, int numCols, double[] scanned) {
-        this.view = new double[numRows][numCols];
+
+        view = new double[numRows][numCols];
+        //c is the variable to keep track of the index in Scanned array
         int c=0;
-        for(int r=0; r<view.length; r++){
-            for(int j=0; j<view[r].length; j++){
-                if
+        for (int r = 0; r < numRows; r++) {
+            //of row is even we move left to right
+            if (r % 2 == 0) {
+                for (int j = 0; j < numCols; j++) {
+                    view[r][c] = scanned[c];
+                    c++;
+                }
+            }
+            //if the row is the not even then move right to left
+            else {
+                for (int j = numCols - 1; j >= 0; j--) {
+                    view[r][j] = scanned[c];
+                    c++;
+                }
             }
         }
+
     }
-    public void getView(){
-        for(int r=0; r<view.length; r++){
-            for(int c=0; c<view[r].length; c++){
-                System.out.println(" "+view[r][c]);
+    public double[][] getView(){
+        return view;
+    }
+    public double getAverage(int startRow, int endRow, int startCol, int endCol){
+        double sum=0;
+        int num=0;
+        for(int r=startRow; r<endRow+1; r++){
+            for(int c = startCol; c<endCol+1; c++){
+                sum = sum+view[r][c];
+                num++;
             }
         }
+        return (sum)/num;
+
     }
 }
